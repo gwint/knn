2c509ea63314da5e6dd4248290a086bbc66176bc
Gregory Wint <gregwint95@gmail.com>
2023-02-16 21:38:30 -0500

    Add utility functions for confusion matrix

--- src/statsutils.cpp
+++ src/statsutils.cpp
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <unordered_set>
 
+const int POSITIVE_LABEL_INDEX = 0;
+const int NEGATIVE_LABEL_INDEX = 1;
+
 std::vector<Data> StatsUtils::getSplitData(const Data& data, const Data& classifications, double testProportion) {
 	std::vector<Data> splitData;
 	assert(data.getNumSamples() == classifications.getNumSamples());
@@ -49,35 +52,65 @@ std::vector<Data> StatsUtils::getSplitData(const Data& data, const Data& classif
 	return std::vector<Data>{trainingData, trainingClassifications, testData, testClassifications};
 }
 
-std::vector<std::vector<double>> StatsUtils::getConfusionMatrix(const Data& testClassifications, const Data& actualClassifications) {
+std::vector<std::vector<double>> StatsUtils::getConfusionMatrix(const Data& testClassifications, const Data& actualClassifications, const std::vector<int>& labels) {
 	assert(testClassifications.getNumSamples() == actualClassifications.getNumSamples());
 	assert(testClassifications.getInputVectorsImmutable().size() == actualClassifications.getInputVectorsImmutable().size());
 
-	double truePositiveProportion = _getTruePositiveProportion(actualClassifications, testClassifications);
+	double truePositiveProportion = _getTruePositiveProportion(actualClassifications, testClassifications, labels);
 
-	double falsePositiveProportion = _getFalsePositiveProportion(actualClassifications, testClassifications);
+	double falsePositiveProportion = _getFalsePositiveProportion(actualClassifications, testClassifications, labels);
 
-	double trueNegativeProportion = _getTrueNegativeProportion(actualClassifications, testClassifications);
+	double trueNegativeProportion = _getTrueNegativeProportion(actualClassifications, testClassifications, labels);
 
-	double falseNegativeProportion = _getFalseNegativeProportion(actualClassifications, testClassifications);
+	double falseNegativeProportion = _getFalseNegativeProportion(actualClassifications, testClassifications, labels);
 	
 	return std::vector<std::vector<double>>{std::vector<double>{truePositiveProportion, falsePositiveProportion}, std::vector<double>{falseNegativeProportion, trueNegativeProportion}};
 }
 
-double StatsUtils::_getTrueNegativeProportion(const Data& actual, const Data& test) {
-	return 0.0;
+double StatsUtils::_getTrueNegativeProportion(const Data& actual, const Data& test, const std::vector<int>& labels) {
+	int numBenignSamples = 0;
+	int numBenignAndPredictedBenignSamples = 0;
+
+	for(int sampleIndex = 0; sampleIndex < actual.getNumSamples(); ++sampleIndex) {
+		int testClassification = test.getSample(sampleIndex).getValue(std::vector<int>{0});
+		int actualClassification = actual.getSample(sampleIndex).getValue(std::vector<int>{0});
+
+		if(testClassification == labels[NEGATIVE_LABEL_INDEX]) {
+			++numBenignSamples;
+			if(actualClassification == labels[NEGATIVE_LABEL_INDEX]) {
+				++numBenignAndPredictedBenignSamples;
+			}
+		}
+	}
+
+	return numBenignSamples / numBenignAndPredictedBenignSamples;
 }
 
-double StatsUtils::_getFalseNegativeProportion(const Data& actual, const Data& test) {
-	return 0.0;
+double StatsUtils::_getFalseNegativeProportion(const Data& actual, const Data& test, const std::vector<int>& labels) {
+	return 1 - _getTruePositiveProportion(actual, test, labels);
 }
 
-double StatsUtils::_getTruePositiveProportion(const Data& actual, const Data& test) {
-	return 0.0;
+double StatsUtils::_getTruePositiveProportion(const Data& actual, const Data& test, const std::vector<int>& labels) {
+	int numMalignantSamples = 0;
+	int numMalignantAndPredictedMalignantSamples = 0;
+
+	for(int sampleIndex = 0; sampleIndex < actual.getNumSamples(); ++sampleIndex) {
+		int testClassification = test.getSample(sampleIndex).getValue(std::vector<int>{0});
+		int actualClassification = actual.getSample(sampleIndex).getValue(std::vector<int>{0});
+
+		if(testClassification == labels[POSITIVE_LABEL_INDEX]) {
+			++numMalignantSamples;
+			if(actualClassification == labels[POSITIVE_LABEL_INDEX]) {
+				++numMalignantAndPredictedMalignantSamples;
+			}
+		}
+	}
+
+	return numMalignantSamples / numMalignantAndPredictedMalignantSamples;
 }
 
-double StatsUtils::_getFalsePositiveProportion(const Data& actual, const Data& test) {
-	return 0.0;
+double StatsUtils::_getFalsePositiveProportion(const Data& actual, const Data& test, const std::vector<int>& labels) {
+	return 1 - _getTrueNegativeProportion(actual, test, labels);
 }
 
 
--- src/statsutils.hpp
+++ src/statsutils.hpp
@@ -8,12 +8,13 @@
 class StatsUtils {
 	public:
 		static std::vector<Data> getSplitData(const Data& data, const Data& classifications, double testProportions);
-		static std::vector<std::vector<double>> getConfusionMatrix(const Data&, const Data&);
+		static std::vector<std::vector<double>> getConfusionMatrix(const Data&, const Data&, const std::vector<int>& labels);
+
 	private:
-		static double _getTrueNegativeProportion(const Data& actual, const Data& test);
-		static double _getTruePositiveProportion(const Data& actual, const Data& test);
-		static double _getFalseNegativeProportion(const Data& actual, const Data& test);
-		static double _getFalsePositiveProportion(const Data& actual, const Data& test);
+		static double _getTrueNegativeProportion(const Data& actual, const Data& test, const std::vector<int>&);
+		static double _getTruePositiveProportion(const Data& actual, const Data& test, const std::vector<int>&);
+		static double _getFalseNegativeProportion(const Data& actual, const Data& test, const std::vector<int>&);
+		static double _getFalsePositiveProportion(const Data& actual, const Data& test, const std::vector<int>&);
 
 };
 #endif
